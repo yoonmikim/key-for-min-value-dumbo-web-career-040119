47e0d3924c045ddadc7178f191a74f092fe0ed6b
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-21 22:34:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest_k = nil
+  smallest_v = nil 
+  name_hash.each do |k, v| 
+    if (v < smallest_v || smallest_v == nil) 
+      smallest_v = v
+      smallest_k = k
+    end
+  end
+  smallest_k
 end
