13666f971c9c3124ce28f2329398febee39f17f5
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-21 22:36:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest_k = nil
   smallest_v = nil 
   name_hash.each do |k, v| 
-    if (v < smallest_v || smallest_v == nil) 
+    if smallest_v == nil || v < smallest_v
       smallest_v = v
       smallest_k = k
     end
